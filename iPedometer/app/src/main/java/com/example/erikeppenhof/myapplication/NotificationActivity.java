e1aaa1e136298daeafb902e172d49cc17bf802ab
Sennaa <senna_vaniersel@hotmail.com>
2015-06-01 15:49:59 +0200

    small changes
    deleted wrong method, added some todo's

---
+++ iPedometer/app/src/main/java/com/example/erikeppenhof/myapplication/NotificationActivity.java
@@ -0,0 +1,95 @@
+package com.example.erikeppenhof.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.erikeppenhof.myapplication.R;
+
+public class NotificationActivity extends ActionBarActivity {
+
+    final Context context = this;
+
+    private static final int DISPLAY_DATA = 1;
+
+    private int snoozetime = 60000*30; // half an hour
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == DISPLAY_DATA) {
+                onCreateDialog().show();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        // SHOW POP_UP
+        Dialog dialog = onCreateDialog();
+        dialog.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notification, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Dialog onCreateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // TODO: Cancel (save in database)
+            }
+        });
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // TODO: OK (save in database)
+            }
+        });
+        builder.setNeutralButton(R.string.snooze, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // Snooze
+                //TODO: save in database
+                mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, snoozetime);
+            }
+        });
+        return builder.create();
+    }
+}
